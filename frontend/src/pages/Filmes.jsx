ab42bcc9f76c12ca53e5271de84a48288b985a77
Isabelly Vaz <belly360.isa@gmail.com>
2024-08-20 20:13:14 -0300

    test

--- frontend/src/pages/Filmes.jsx
+++ frontend/src/pages/Filmes.jsx
@@ -3,10 +3,30 @@ import { Footer } from "../components/Footer"
 import { NavLink } from "react-router-dom"
 import style from "./filmes.module.css"
 
+
 export function Filmes() {
     return(
         <>
             <Header />
+            <div className={style.pesquisafundo}>
+                <div className={style.pesquisa}>
+                <input className={style.barra}
+                    type="text" 
+                    placeholder="Pesquisar filmes"
+                />
+                <button className={style.botaopesquisa}>
+                    <img src="src/assets/Botao-Pesquisa.svg" alt="Pesquisar" />
+                </button>
+                </div>
+                <div className={style.filtros}>
+                    <input type="text" className={style.filter}/>
+                    <input type="text" className={style.filter}/>
+                </div>
+            </div>
+
+            <div></div>
+
+            <div></div>
             <Footer />
         </>
     )
