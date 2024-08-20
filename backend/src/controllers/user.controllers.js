32d3402233900e66faf971702f7d34af2686b4eb
João Gabriel Ramos <jgbvramos@id.uff.br>
2024-08-20 19:11:00 -0300

    feat: criando arq para registrar usuário

---
+++ backend/src/controllers/user.controllers.js
@@ -0,0 +1,45 @@
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
+
+const registerUser = async (req, res) => {
+    const { name, lastName, cpf, birthday, username, email, password } = req.body;
+
+    try {
+        // Verifique se o e-mail, CPF ou nome de usuário já estão em uso
+        const existingUser = await User.findOne({ where: { email } });
+        const existingCpf = await User.findOne({ where: { cpf } });
+        const existingUsername = await User.findOne({ where: { username }});
+      
+        if (existingUser) {
+            return res.status(400).json({ message: 'E-mail já cadastrado.' });
+        }
+        else if (existingCpf) {
+            return res.status(400).json({ message: 'CPF já cadastrado.' });
+        }
+        else if (existingUsername) {
+            return res.status(400).json({ message: 'Nome de usuário já existente.' });
+        }
+
+        // Criptografe a senha
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // Crie o novo usuário
+        const user = await User.create({
+            name,
+            lastName,
+            cpf,
+            birthday,
+            username,
+            email,
+            password: hashedPassword,
+        });
+
+        // Resposta de sucesso
+        return res.status(201).json({ message: 'Usuário registrado com sucesso!' });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Erro ao registrar usuário' });
+    }
+};
+
+module.exports = { registerUser };
