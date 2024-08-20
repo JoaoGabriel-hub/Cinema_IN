d8204ecccf77849b2c52d2580b1afd79c4a0b5ca
Isabelly <belly360.isa@gmail.com>
2024-08-19 22:15:03 -0300

    Home

--- Cinema/src/components/Header.jsx
+++ Cinema/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import style from "./header.module.css"
+import { NavLink } from "react-router-dom"
 
 export function Header() {
     return(
@@ -6,15 +7,21 @@ export function Header() {
             <div className={style.header}>
                 <img src="src/assets/Logo.svg" className={style.logo} />
                 <div className={style.buttons}>
-                    <button>
-                        <img src="src/assets/Icone-Filmes.svg"/>
-                    </button>
-                    <button>
-                        <img src="src/assets/Icone-Entrar.svg"/>
-                    </button>
-                    <button>
-                        <img src="src/assets/Icone-Ajuda.svg"/>
-                    </button>
+                    <NavLink to="/filmes">
+                        <button>
+                            <img src="src/assets/Icone-Filmes.svg"/>
+                        </button>
+                    </NavLink>
+                    <NavLink to="/login">
+                        <button>
+                            <img src="src/assets/Icone-Entrar.svg"/>
+                        </button>
+                    </NavLink>
+                    <NavLink to="/faleconosco">
+                        <button>
+                            <img src="src/assets/Icone-Ajuda.svg"/>
+                        </button>
+                    </NavLink>
                 </div>
             </div>
         </div>
