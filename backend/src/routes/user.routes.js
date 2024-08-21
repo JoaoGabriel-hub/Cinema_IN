c7bb2752f9f5808a4970fa72d6a8d95553c0e785
João Gabriel Ramos <jgbvramos@id.uff.br>
2024-08-21 02:23:29 -0300

    feat: adc rota para logar usuário

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const userRoutes = express.Router();
 
-const { registerUser } = require('../controllers/user.controllers');
+const { registerUser, loginUser } = require('../controllers/user.controllers');
 
 // Rota para registrar um usuário
-userRoutes.post('/', registerUser);
+userRoutes.post('/register', registerUser);
+
+// Rota para logar um usuário existente
+userRoutes.post('/login', loginUser);
 
 module.exports = userRoutes;
