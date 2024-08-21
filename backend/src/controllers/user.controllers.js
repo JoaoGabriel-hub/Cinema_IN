a71449f85dda999e6c3552336e4c80b6aceeac7a
João Gabriel Ramos <jgbvramos@id.uff.br>
2024-08-21 02:23:01 -0300

    feat: adc const para logar usuário

--- backend/src/controllers/user.controllers.js
+++ backend/src/controllers/user.controllers.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt');
 const { User } = require('../models/models');  
+const Sequelize = require('sequelize');
 
 
 const registerUser = async (req, res) => {
@@ -42,4 +43,33 @@ const registerUser = async (req, res) => {
     }
 };
 
-module.exports = { registerUser };
+const loginUser = async (req, res) => {
+    const { usernameOrEmail, password } = req.body;
+
+    try {
+        // Verificando se o usuário existe pelo nome de usuário ou e-mail
+        const user = await User.findOne({
+            where: {
+                [Sequelize.Op.or]: [{ username: usernameOrEmail }, { email: usernameOrEmail }]
+            }
+        });
+
+        if (!user) {
+            return res.status(400).json({ message: 'Usuário ou e-mail não encontrado.' });
+        }
+
+        // Verificando se a senha está correta
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordValid) {
+            return res.status(400).json({ message: 'Senha incorreta.' });
+        }
+
+        return res.status(201).json({ message: 'Usuário logado com sucesso!' });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Erro ao realizar login' });
+    }
+};
+
+module.exports = { registerUser, loginUser };
