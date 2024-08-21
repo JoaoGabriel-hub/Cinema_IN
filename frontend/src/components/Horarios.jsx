c1b90297180d6fbe116f4fbe69098bdad923cbbb
Isabelly Vaz <belly360.isa@gmail.com>
2024-08-21 02:00:20 -0300

    style: Sessões

--- frontend/src/components/Horarios.jsx
+++ frontend/src/components/Horarios.jsx
@@ -1,8 +1,14 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import style from "./horarios.module.css";
 
 export function Horarios() {
     const [activeSession, setActiveSession] = useState(null);
+    const navigate = useNavigate();
+
+    const handleButtonClick = (hora, tipoTela) => {
+        navigate('/checkout', { state: { hora, tipoTela } });
+    };
 
     return (
         <div className={style.corpohorarios}>
@@ -15,30 +21,30 @@ export function Horarios() {
             <div className={style.sessoes} id="2D" style={{ display: activeSession === '2D' || activeSession === null ? 'block' : 'none' }}>
                 <div className={style.titulo}>2D</div>
                 <div className={style.tdhora}>
-                    <button className={style.botaohorario}>9:20</button>
-                    <button className={style.botaohorario}>12:50</button>
-                    <button className={style.botaohorario}>17:30</button>
-                    <button className={style.botaohorario}>21:20</button>
+                    <button className={style.botaohorario} onClick={() => handleButtonClick('9:20', '2D')}>9:20</button>
+                    <button className={style.botaohorario} onClick={() => handleButtonClick('12:50', '2D')}>12:50</button>
+                    <button className={style.botaohorario} onClick={() => handleButtonClick('17:30', '2D')}>17:30</button>
+                    <button className={style.botaohorario} onClick={() => handleButtonClick('21:20', '2D')}>21:20</button>
                 </div>
             </div>
 
             <div className={style.sessoes} id="3D" style={{ display: activeSession === '3D' || activeSession === null ? 'block' : 'none' }}>
                 <div className={style.titulo}>3D</div>
                 <div className={style.tdhora}>
-                    <button className={style.botaohorario}>13:40</button>
-                    <button className={style.botaohorario}>15:00</button>
-                    <button className={style.botaohorario}>19:10</button>
-                    <button className={style.botaohorario}>22:30</button>
+                    <button className={style.botaohorario} onClick={() => handleButtonClick('13:40', '3D')}>13:40</button>
+                    <button className={style.botaohorario} onClick={() => handleButtonClick('15:00', '3D')}>15:00</button>
+                    <button className={style.botaohorario} onClick={() => handleButtonClick('19:10', '3D')}>19:10</button>
+                    <button className={style.botaohorario} onClick={() => handleButtonClick('22:30', '3D')}>22:30</button>
                 </div>
             </div>
 
             <div className={style.sessoes} id="IMAX" style={{ display: activeSession === 'IMAX' || activeSession === null ? 'block' : 'none' }}>
                 <div className={style.titulo}>IMAX</div>
                 <div className={style.tdhora}>
-                    <button className={style.botaohorario}>14:50</button>
-                    <button className={style.botaohorario}>16:10</button>
-                    <button className={style.botaohorario}>18:00</button>
-                    <button className={style.botaohorario}>20:20</button>
+                    <button className={style.botaohorario} onClick={() => handleButtonClick('14:50', 'IMAX')}>14:50</button>
+                    <button className={style.botaohorario} onClick={() => handleButtonClick('16:10', 'IMAX')}>16:10</button>
+                    <button className={style.botaohorario} onClick={() => handleButtonClick('18:00', 'IMAX')}>18:00</button>
+                    <button className={style.botaohorario} onClick={() => handleButtonClick('20:20', 'IMAX')}>20:20</button>
                 </div>
             </div>
         </div>
--- frontend/src/pages/Sessões.jsx
+++ frontend/src/pages/Sessões.jsx
@@ -1,15 +1,66 @@
-import { Header } from "../components/Header"
-import { Footer } from "../components/Footer"
-import { NavLink } from "react-router-dom"
-import style from "./sessões.module.css"
-import { Horarios } from "../components/Horarios"
+import React, { useState } from 'react';
+import { Header } from "../components/Header";
+import { Footer } from "../components/Footer";
+import { NavLink } from "react-router-dom";
+import style from "./sessões.module.css";
+import { Horarios } from "../components/Horarios";
 
 export function Sessões() {
-    return(
+    const [cidade, setCidade] = useState('');
+    const [bairro, setBairro] = useState('');
+
+    const cidades = ['Niterói, RJ', 'Nova Iguaçu, RJ', 'Rio de Janeiro, RJ', 'São Gonçalo, RJ', 'Petrópolis, RJ'];
+    const bairros = {
+        'Niterói, RJ': ['Viradouro','Barreato', 'Badu', 'Muriqui'],
+        'Nova Iguaçu, RJ': ['Centro', 'Rosa dos Ventos', 'Comendador Soares'],
+        'Rio de Janeiro, RJ': ['Botafogo', 'Leblon', 'Campo Grande'],
+        'São Gonçalo, RJ': ['Trindade','Rocha', 'Salgueiro'],
+        'Petrópolis, RJ': ['Moinho Preto','Vila Manzini', '24 de Maio']
+    };
+
+    return (
         <>
             <Header />
-            <Horarios/>
+            <div className={style.banner}>
+                <img src="o" className={style.capadofilme} />
+                <div className={style.informacoes}>
+                    <div className={style.dadosfilme}>
+                        <div className={style.topo}>
+                            <h1>Nome do Filme</h1>
+                            <img src="s" className={style.idade} />
+                        </div>
+                        <h2>Estilos do filme</h2>
+                        <p>
+                            Resumo do filme suuuuuper legal! É sério!
+                            Lorem ipsum dolor sit amet consectetur 
+                            adipisicing elit. Hic adipisci sunt eveniet 
+                            aliquam odio est, debitis reprehenderit alias 
+                            officiis ut magnam necessitatibus dolor et ea 
+                            ullam cum ab vel vero?
+                        </p>
+                    </div>
+                    <div className={style.lugar}>
+                        <select className={style.filter} name="Cidade" value={cidade} onChange={(e) => setCidade(e.target.value)}>
+                            <option disabled value="">Cidade</option>
+                            {cidades.map((cidade) => (
+                                <option key={cidade} value={cidade}>
+                                    {cidade}
+                                </option>
+                            ))}
+                        </select>
+                        <select className={style.filter} name="Bairro" value={bairro} onChange={(e) => setBairro(e.target.value)} disabled={!cidade}>
+                            <option disabled value="">Bairro</option>
+                            {cidade && bairros[cidade].map((bairro) => (
+                                <option key={bairro} value={bairro}>
+                                    {bairro}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <Horarios />
             <Footer />
         </>
-    )
+    );
 }
