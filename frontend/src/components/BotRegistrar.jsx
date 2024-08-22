ae235607ca25fb2e406c8b97991cf42b0cc775b6
Isabelly Vaz <belly360.isa@gmail.com>
2024-08-21 22:45:15 -0300

    style: Botão de registro

---
+++ frontend/src/components/BotRegistrar.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import style from "./botregistrar.module.css";
+import {NavLink} from "react-router-dom"
+
+export default function BotRegistrar() {
+   
+    const [modal, setModal] = useState(false);
+
+    const toggleModal = () => {
+        setModal(!modal)
+    }
+
+  return (
+    <div>
+
+        <button onClick={toggleModal} className={style.open}>REGISTRAR</button>
+      
+       {modal &&(
+            <div className={style.modal}>
+                <div className={style.overlay}></div>
+                <div className={style.content}>
+                    <div>
+                    <NavLink to='/login'>
+                        <button className={style.close}>
+                            <img src="src/assets/Botao.svg"/>
+                        </button>
+                    </NavLink>
+                    </div>
+                    <div className={style.text}>
+                        <h1>Cadastro Criado!</h1>
+                        <h2>Bem-Vindo à Nossa Comunidade Cinematográfica!</h2>
+                        <p>
+                        Obrigado por se juntar a nós na nossa comunidade 
+                        cinematográfica. Sua jornada para uma experiência 
+                        cinematográfica única começa agora. <br /><br />
+                        Você será redirecionado em instantes 
+                        para página de login em instantes.
+                        </p>
+                    </div>
+                    
+                </div>
+            </div>
+      )}
+        
+    </div>
+  );
+  }
--- frontend/src/pages/Registrar.jsx
+++ frontend/src/pages/Registrar.jsx
@@ -2,11 +2,87 @@ import { Header } from "../components/Header"
 import { Footer } from "../components/Footer"
 import { NavLink } from "react-router-dom"
 import style from "./registrar.module.css"
+import BotRegistrar from "../components/BotRegistrar.jsx"
 
 export function Registrar() {
     return(
         <>
             <Header />
+            <div className={style.corporegistro}>
+                <div className={style.apresentacao}>
+                    <h1>
+                        Junte-se à Comunidade Cinematográfica! 
+                        Cadastre-se Aqui!
+                    </h1>
+                    <h2>
+                        Seja bem-vindo à nossa comunidade apaixonada 
+                        pelo mundo do cinema. Ao fazer parte do nosso 
+                        espaço digital, você está prestes a mergulhar 
+                        em uma experiência cinematográfica única, onde a 
+                        magia das telonas ganha vida com um toque moderno.<br></br>
+                        Nosso formulário de cadastro é o primeiro passo para 
+                        embarcar nessa jornada emocionante. Ao preenchê-lo, 
+                        você se tornará um membro da nossa comunidade, onde 
+                        amantes do cinema se reúnem para compartilhar o 
+                        entusiasmo, as emoções e as histórias que permeiam 
+                        cada cena.
+                    </h2>
+                </div>
+                
+                <div className={style.formulario}>
+                    <h1>Registre-se</h1>
+                    <input 
+                        type="text" 
+                        placeholder="Nome" 
+                        name="nome"
+                        required
+                    />
+                    <input 
+                        type="text" 
+                        placeholder="Sobrenome" 
+                        name="sobrenome"
+                        required
+                    />
+                    <input 
+                        type="text" 
+                        placeholder="CPF" 
+                        name="cpf"  
+                        required
+                    />
+                    <input 
+                        type="data" 
+                        placeholder="Data de nascimento" 
+                        name="data-nascimento"
+                        required
+                    />
+                    <input 
+                        type="text" 
+                        placeholder="Nome de Usuário" 
+                        name="nome-usuario"
+                        required
+                    />
+                    <input 
+                        type="email"
+                        placeholder="E-Mail" 
+                        name="email"
+                     />
+                    <input 
+                        type="password" 
+                        placeholder="Senha" 
+                        name="senha"
+                    />
+                    <input 
+                        type="password" 
+                        placeholder="Confirmar senha" 
+                        name="confirmar-senha"
+                        required
+                    />
+                
+                    <div className={style.botaoregistrar}>
+                        <BotRegistrar/>
+                    </div>
+                </div>
+            </div>
             <Footer />
         </>
     )
